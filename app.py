9645ffc5ffd7c46bb32db405d86270e8d3742925
Izza Shahzad <142139158+izza013@users.noreply.github.com>
2026-05-03 14:10:57 +0500

    Fix image filename in weapon detection app
    Updated image filename from 'v.png' to 'v.JPG'.

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ model = load_model()
 # -------------------------------
 st.title("Weapon Detection (YOLOv10)")
 st.image(
-    "v.png",
+    "v.JPG",
     caption="Weapon Detection using YOLOv10",
     use_column_width=True
 )
